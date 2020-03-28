dd9737432c8e0184ae6ebf9fdf2947c54c24387c
Ibiyemi Abiodun <ibiyemi@intulon.com>
2020-03-28 17:20:37 -0400

    added more rules to base

--- base/index.js
+++ base/index.js
@@ -10,7 +10,17 @@ module.exports = {
     'eslint:recommended',
   ],
   rules: {
+    // semantic rules
+    'accessor-pairs': ['error'],
+    'grouped-accessor-pairs': ['error', 'getBeforeSet'],
+    'yoda': ['error', 'never', { exceptRange: true }],
+    'no-unneeded-ternary': ['error'],
+    'no-var': ['error'],
+    'no-useless-computed-key': ['error'],
+    'prefer-const': ['error', { destructuring: 'all' }],
+    
     // spacing rules
+    'dot-location': ['warn', 'property'],
     'block-spacing': ['error', 'always'],
     'key-spacing': ['error'],
     'keyword-spacing': ['error', { before: true, after: true }],
@@ -21,8 +31,14 @@ module.exports = {
     'space-before-blocks': ['error', 'always'],
     'no-whitespace-before-property': ['error'],
     'func-call-spacing': ['error', 'never'],
+    'generator-star-spacing': ['error', 'after'],
+    'yield-star-spacing': ['error', 'after'],
     'indent': ['error', 2],
     
+    // newline rules
+    'object-curly-newline': ['error', 'always', { multiline: true, minProperties: 4, consistent: true }],
+    'object-property-newline': ['error', 'always', { allowAllPropertiesOnSameLine: true }],
+
     // comma rules
     'comma-dangle': ['error', 'never'],
     'comma-spacing': ['error', { before: false, after: true }],
